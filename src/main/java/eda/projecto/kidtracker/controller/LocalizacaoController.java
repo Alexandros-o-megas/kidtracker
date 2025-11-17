3aab3c287a4574665c2060413172591b50689198
Alexandros-o-megas <quivealexandre@gmail.com>
2025-11-17 17:10:25 +0200

    Add CrossOrigin annotation to LocalizacaoController for CORS support

--- src/main/java/eda/projecto/kidtracker/controller/LocalizacaoController.java
+++ src/main/java/eda/projecto/kidtracker/controller/LocalizacaoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/localizacoes")
+@CrossOrigin(origins = "http://localhost:49202")
 @RequiredArgsConstructor
 public class LocalizacaoController {
 
