fb657efc7746f8512592cbeff1e3b1f5f68b5c37
Alexandros-o-megas <quivealexandre@gmail.com>
2025-11-17 11:46:08 +0200

    Initialize project structure with Maven, including essential configuration files and application setup

---
+++ src/main/java/eda/projecto/kidtracker/KidtrackerApplication.java
@@ -0,0 +1,13 @@
+package eda.projecto.kidtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eda/projecto/kidtracker/KidtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package eda.projecto.kidtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
